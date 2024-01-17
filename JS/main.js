3029424fc75cfa034e9756ea546d472ea7d6dfb5
Cristian-DW <cristtiiank@gmail.com>
2024-01-16 20:28:52 -0500

    feat: :sparkles: functionality added to start button
    functionality added to the start button with the help of nested arrays

--- JS/main.js
+++ JS/main.js
@@ -3,53 +3,53 @@ const scoreBoard = document.getElementById('score');
 const startButton = document.getElementById('start');
 const gameOverSign = document.getElementById('gameOver');
 
-// game settingns
+// Configuración del juego
 
 const boardSize = 10;
 const gameSpeed = 100;
 const squareTypes = {
-   emptySquare: 0,
-   snakeSquare: 1,
-   foodSquare: 2
+  emptySquare: 0,
+  snakeSquare: 1,
+  foodSquare: 2
 };
 
 const directions = {
-   arrowUp: -10,
-   arrowDown: 10,
-   arrowLeft: -1,
-   arrowRight: 1
+  arrowUp: -10,
+  arrowDown: 10,
+  arrowLeft: -1,
+  arrowRight: 1
 };
 
-// game variables
+// Variables del juego
 let snake, score, direction, boardSquares, emptySquares, moveInterval;
 
 const createBoard = () => {
-   boardSquares.forEach((row, rowIndex) => {
-      row.forEach((column, columnIndex) => {
-         const squareIndex = `${rowIndex}${columnIndex}`;
-         const squareElement = document.createElement('div');
-         squareElement.setAttribute('class', 'square emptySquare');
-         squareElement.setAttribute('id', squareValue);
-         board.appendChild(squareElement);
-         emptySquares.push(squareValue);
-      })
-      
-   });
+  boardSquares.forEach((row, rowIndex) => {
+    row.forEach((column, columnIndex) => {
+      const squareIndex = `${rowIndex}${columnIndex}`;
+      const squareElement = document.createElement('div');
+      const squareValue = squareTypes.emptySquare;
+      squareElement.setAttribute('class', 'square emptySquare');
+      squareElement.setAttribute('id', squareIndex);
+      board.appendChild(squareElement);
+      emptySquares.push(squareValue);
+    });
+  });
 };
 
 const setGame = () => {
-   snake = [ '00', '01', '02', '03' ];
-   score = snake.length;
-   direction = 'arrowRight';
-   boardSquares = array.from(array(boardSize), () => new array(boardSize).fill(squareTypes.emptySquare));
-   board.innerHTML = '';
-   emptySquares = [];
-   createBoard();
-}
+  snake = ['00', '01', '02', '03'];
+  score = snake.length;
+  direction = 'arrowRight';
+  boardSquares = Array.from(Array(boardSize), () => new Array(boardSize).fill(squareTypes.emptySquare));
+  board.innerHTML = '';
+  emptySquares = [];
+  createBoard();
+};
 
 const startGame = () => {
-   setGame();
-}
+  setGame();
+  
+};
 
 startButton.addEventListener('click', startGame);
-
